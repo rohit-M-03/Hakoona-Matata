9ab7921364359bec2d330541367d584323303abe
rohit-M-03 <103727915+rohit-M-03@users.noreply.github.com>
2024-04-06 00:32:53 +0530

    Update upi.py

--- upi.py
+++ upi.py
@@ -1,14 +1,22 @@
 
 from bhimupipy import verify_upi
 
-def upi(st):
-    global upivalue
-    a=verify_upi(st)
-    upivalue=a["data"]["vpaValid"]
+import sys
 
-def upicheck(st):
-    return upi(st)
-    
+def process_arguments(args):
+  
+    if len(args) > 1:
+        global upivalue
+        upi_id = args[1]
+        print("UPI ID passed as argument:", upi_id)
+        a=verify_upi(upi_id)
+        upivalue=a["data"]["vpaValid"]
+       
+    else:
+        print("No UPI ID provided as argument.")
+
+def main():
+    process_arguments(sys.argv)
+
 if __name__ == "__main__":
-    st=sys.argv[0]
-    upi(st)
+    main()
